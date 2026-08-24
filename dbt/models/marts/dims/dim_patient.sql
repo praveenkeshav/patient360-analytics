4c3379bec317afd805dc50eb3a9f7abd2583d42a
Praveen Keshav <prvnkshv@gmail.com>
2026-08-24 10:42:57 +0530

    feat: add patient status to patient dimension

--- dbt/models/marts/dims/dim_patient.sql
+++ dbt/models/marts/dims/dim_patient.sql
@@ -20,6 +20,11 @@ select
     ethnicity,
     is_deceased,
 
+    case
+        when is_deceased then 'DECEASED'
+        else 'ACTIVE'
+    end as patient_status,
+
     current_timestamp() as _loaded_at,
     '{{ invocation_id }}' as _dbt_invocation_id,
     'STAGING.STG_PATIENTS' as _record_source
